32218b9f960c3d326b2f4e51fb7a5a6a3002a969
groemhildt <groemhildt@wsbeng.com>
2020-04-21 13:51:37 -0500

    verion

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='arcpy_dbgrate',
     author='roemhildtg',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
