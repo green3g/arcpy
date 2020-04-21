6d06bcceccd9427390112f632888fbdfa1aa4457
groemhildt <groemhildt@wsbeng.com>
2020-04-21 13:51:06 -0500

    fix cli

--- arcpy_dbgrate/main.py
+++ arcpy_dbgrate/main.py
@@ -5,7 +5,7 @@ from os.path import join
 from sys import path
 from os import getcwd, environ
 from arcpy_dbgrate.compare_models import generate_migration
-from arcpy_dbgrate.create_models import create_models
+from arcpy_dbgrate.create_models import create_models as _create_models
 from dbgrate.main import cli 
 
 # modify the local path so we can import our env.py
@@ -13,7 +13,8 @@ WORKING_DIR = getcwd()
 path.append(WORKING_DIR)
 
 @cli.command()
-create_models
+def create_models():
+    _create_models()
 
 if __name__ == '__main__':
     logging.info('arcpy: Current working directory is {}'.format(WORKING_DIR))
