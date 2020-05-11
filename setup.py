128f5c30fa76f742ec638d73a1b01cbf1d899397
groemhildt <groemhildt@wsbeng.com>
2020-05-11 14:05:51 -0500

    v bumps

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='arcpy_dbgrate',
     author='roemhildtg',
-    version='0.2.0',
+    version='0.3.0',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
